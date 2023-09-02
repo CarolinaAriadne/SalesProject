e0dabf7519cd3dd874608637b1e66913fc9c7346
Carolina Ariadne <c.ariadneadv@gmail.com>
2023-09-02 16:20:30 -0300

    Seller details and eager loading

--- SalesProject/Services/SellerService.cs
+++ SalesProject/Services/SellerService.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using SalesProject.Models;
+using Microsoft.EntityFrameworkCore;
 
 
 namespace SalesProject.Services
@@ -30,7 +31,7 @@ namespace SalesProject.Services
 
         public Seller FindById(int id)
         {
-            return _context.Seller.FirstOrDefault(obj => obj.Id == id);
+            return _context.Seller.Include(obj => obj.Departament).FirstOrDefault(obj => obj.Id == id);
         }
 
         public void Remove(int id)
