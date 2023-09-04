9b98ee0c8f6369432eb1c3ec2cb8f4beb0b72fd0
Carolina Ariadne <c.ariadneadv@gmail.com>
2023-09-03 22:46:20 -0300

    preparing sales search navigation views

---
+++ SalesProject/Controllers/SalesRecordsController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using SalesProject.Models;
+using SalesProject.Services;
+using SalesProject.Models.ViewModels;
+using System.Collections.Generic;
+using SalesProject.Services.Exceptions;
+using System.Diagnostics;
+
+namespace SalesProject.Controllers
+{
+    public class SalesRecordsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult SimpleSearch()
+        {
+            return View();
+        }
+        public IActionResult GroupingSearch()
+        {
+            return View();
+        }
+    }
+}
