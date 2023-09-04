1efe99e152d5152fa94cb69d08f4b7701fbeb7c8
Carolina Ariadne <c.ariadneadv@gmail.com>
2023-09-03 22:27:49 -0300

    Exception handling for delete

---
+++ SalesProject/Services/Exceptions/IntegrityException.cs
@@ -0,0 +1,23 @@
+﻿
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SalesProject.Models;
+
+
+
+
+namespace SalesProject.Services.Exceptions
+{
+    public class IntegrityException : ApplicationException
+    {
+
+        public IntegrityException(string message) : base(message)
+        {
+
+        }
+
+    }
+}
